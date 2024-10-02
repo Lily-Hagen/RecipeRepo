4484a3ae1140b7b2eff92d446e4a5049c301bb5a
JMRichmond99 <144260502+jaredr3UMBC@users.noreply.github.com>
2024-10-02 09:10:23 -0400

    Sprint1Setup
    Setup up templating for site. Added new html template files for a couple of pages that will need to be setup eventually.
    Started working on some frontend design for Home Page and SignUp Page.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Hello World<h1>"
+    return render_template("Home.html")
+
+
+@views.route('/MyCookbooks')
+def Cookbooks():
+    return render_template("MyCookbooks.html")
+
+
+@views.route('/Explore')
+def Explore():
+    return render_template("Explore.html")
