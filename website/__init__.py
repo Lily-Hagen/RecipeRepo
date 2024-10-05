e9a7e0dbd6224f675e0548ffea8f8a133a347c32
JMRichmond99 <144260502+jaredr3UMBC@users.noreply.github.com>
2024-10-04 21:25:30 -0400

    User DB and User SignUp Creation
    Added initial DB setup with the user class created.
    
    Implemented the Signup API call for creating a User, with proper validation checks.

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'M@cr0M@rt'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,4 +18,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User
+
+    create_database(app)
+
     return app
+
+#Checking for the path might be redundant code so we can research some more and see if its needed.
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -10,6 +13,26 @@ def login():
 def logout():
     return "<h1>Logout<h1>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
+    data = request.form
+    if (request.method == 'POST'):
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user_exists = User.query.filter_by(email=email).first()
+        if (user_exists):
+            #Post error message saying user with that email alreay exists
+            flash('Email Provided Already Has Account!', category='error')
+        else:
+            #create user
+            new_user = User(email=email, first_name=firstname, last_name=lastname, password=generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account Created!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("Signup.html")
